cee5b1b9f4a580ae96b4f5953dd167d8abcbf313
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2023-06-24 22:07:57 +0530

    Updated mergesort.c by adding malloc()
    I noticed a minor improvement that can be made. 
    Instead of declaring the temporary subarrays L and M inside the merge function, it would be better to allocate memory dynamically using malloc(). 
    This change ensures that the subarrays can handle larger input sizes.

--- sorting/updated_merge_sort.c
+++ sorting/updated_merge_sort.c
@@ -1,29 +1,22 @@
-// Merge sort in C
-
 #include <stdio.h>
+#include <stdlib.h>
 
-// Merge two subarrays L and M into arr
 void merge(int arr[], int p, int q, int r) {
-
-  // Create L ← A[p..q] and M ← A[q+1..r]
   int n1 = q - p + 1;
   int n2 = r - q;
 
-  int L[n1], M[n2];
+  int* L = (int*)malloc(n1 * sizeof(int));
+  int* M = (int*)malloc(n2 * sizeof(int));
 
   for (int i = 0; i < n1; i++)
     L[i] = arr[p + i];
   for (int j = 0; j < n2; j++)
     M[j] = arr[q + 1 + j];
 
-  // Maintain current index of sub-arrays and main array
-  int i, j, k;
-  i = 0;
-  j = 0;
-  k = p;
+  int i = 0;
+  int j = 0;
+  int k = p;
 
-  // Until we reach either end of either L or M, pick larger among
-  // elements L and M and place them in the correct position at A[p..r]
   while (i < n1 && j < n2) {
     if (L[i] <= M[j]) {
       arr[k] = L[i];
@@ -35,8 +28,6 @@ void merge(int arr[], int p, int q, int r) {
     k++;
   }
 
-  // When we run out of elements in either L or M,
-  // pick up the remaining elements and put in A[p..r]
   while (i < n1) {
     arr[k] = L[i];
     i++;
@@ -48,31 +39,26 @@ void merge(int arr[], int p, int q, int r) {
     j++;
     k++;
   }
+
+  free(L);
+  free(M);
 }
 
-// Divide the array into two subarrays, sort them and merge them
 void mergeSort(int arr[], int l, int r) {
   if (l < r) {
-
-    // m is the point where the array is divided into two subarrays
     int m = l + (r - l) / 2;
-
     mergeSort(arr, l, m);
     mergeSort(arr, m + 1, r);
-
-    // Merge the sorted subarrays
     merge(arr, l, m, r);
   }
 }
 
-// Print the array
 void printArray(int arr[], int size) {
   for (int i = 0; i < size; i++)
     printf("%d ", arr[i]);
   printf("\n");
 }
 
-// Driver program
 int main() {
   int arr[] = {6, 5, 12, 10, 9, 1};
   int size = sizeof(arr) / sizeof(arr[0]);
@@ -81,4 +67,5 @@ int main() {
 
   printf("Sorted array: \n");
   printArray(arr, size);
+  return 0;
 }
